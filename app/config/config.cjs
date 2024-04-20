11267e15d9a4cc64b2d93878850285082d8fdc70
Иван Мокрецов <ivan.mokretsov@icloud.com>
2024-04-21 01:53:49 +0300

    ex6

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -13,22 +13,22 @@ module.exports = {
     port: process.env.DATABASE_PORT,
     host: process.env.DATABASE_HOST,
     dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+      // ssl: {
+      //   require: false,
+      //   rejectUnauthorized: false,
+      // },
     },
   },
-  test: {
-    dialect: 'sqlite',
-    storage: './database.test.sqlite',
-  },
   // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
+  //   dialect: 'sqlite',
+  //   storage: './database.test.sqlite',
   // },
+  test: {
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
+  },
 };
